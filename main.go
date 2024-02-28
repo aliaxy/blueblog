25ad75c5c9d13cbed410570056314ff2b5614e86
aliaxy <aruvelut00@163.com>
2024-02-28 14:05:15 +0800

    使用Docker-compose部署

--- main.go
+++ main.go
@@ -18,15 +18,18 @@ func main() {
 		fmt.Println("need config file.eg: blueblog config.yaml")
 		return
 	}
+	fmt.Println(1)
 	if err := settings.Init(os.Args[1]); err != nil {
 		fmt.Println("init settings failed, err:", err.Error())
 		return
 	}
+	fmt.Println(2)
 
 	if err := logger.Init(settings.Conf.LogConfig, settings.Conf.Mode); err != nil {
 		fmt.Println("init logger failed, err:", err.Error())
 		return
 	}
+	fmt.Println(3)
 
 	if err := mysql.Init(settings.Conf.MySQLConfig); err != nil {
 		fmt.Println("init mysql failed, err:", err.Error())
@@ -39,16 +42,19 @@ func main() {
 		return
 	}
 	defer redis.Close()
+	fmt.Println(4)
 
 	if err := snowflake.Init(settings.Conf.StartTime, settings.Conf.MachineID); err != nil {
 		fmt.Println("init snowflake failed, err:", err.Error())
 		return
 	}
+	fmt.Println(5)
 
 	if err := controller.InitTrans("zh"); err != nil {
 		fmt.Println("init validator trans failed, err:", err.Error())
 		return
 	}
+	fmt.Println(6)
 
 	r := router.Setup(settings.Conf.Mode)
 
